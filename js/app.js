5b30c52f6f71e056365ab6948ff528b5f9b992e1
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-08 09:36:19 +0200

    JS create element and add class list append

--- js/app.js
+++ js/app.js
@@ -7,26 +7,36 @@ for (let i = 0; i < 100; i++) {
     
     const n = i + 1;
     
-    let boxString = '<div class="box">' + n + '</div>';
+    // let boxString = '<div class="box">' + n + '</div>';
     // console.log(n)
+
+    let boxDOMElement = document.createElement('div')
+    boxDOMElement.classList.add('box')
+    boxDOMElement.innerHTML = n
     // - Calcolare i multipli di 3 e 5 
     if(n % 3 === 0 && n % 5 === 0) {
-        boxString = '<div class="box">' + "fizzbuzz" + '</div>';
+        // boxString = '<div class="box">' + "fizzbuzz" + '</div>';
+        boxDOMElement.innerHTML = "fizzbuzz"
         console.log("fizzbuzz");
+
 
     // - Calcolare i multipli di 5
     } else if(n % 5 === 0) {
-        boxString = '<div class="box">' + "buzz" + '</div>';
+        // boxString = '<div class="box">' + "buzz" + '</div>';
+        boxDOMElement.innerHTML = "buzz"
         console.log("buzz")
 
     // - Calcolare i multipli di 3
     } else if((n % 3 === 0)) {
-        boxString = '<div class="box">' + "fizz" + '</div>';
+        // boxString = '<div class="box">' + "fizz" + '</div>';
+        boxDOMElement.innerHTML = "fizz"
         console.log("fizz")
 
     } else {
         console.log(n)
+        console.dir(boxDOMElement)
 
     }
-buzzfizzDOMElement.innerHTML += boxString
+// buzzfizzDOMElement.innerHTML += boxString
+    buzzfizzDOMElement.append(boxDOMElement)
 }
